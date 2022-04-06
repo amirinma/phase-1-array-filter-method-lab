422f7fbf48fdacbdd8561de2e1e745ded85ec10a
Mohammad <zia.ro@live.com>
2022-04-06 01:57:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(array, string){
+      const matchingFound = array.filter(function(element){
+          return element.toUpperCase() === string.toUpperCase()
+      }  )
+      return matchingFound
+}
+function fuzzyMatch(array, string){
+    return array.filter(element =>
+        element.indexOf(string) !== -1 && element.charAt(0) === string.charAt(0)
+    )
+}                            
+
+function matchName(drivers, string){
+    return drivers.filter(element => element['name'] === string)
+}
+
